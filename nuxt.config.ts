3d54d6a4e544b5f6269ace978faafd4b8b277a01
Ridho Idris <ridhoidris@gmail.com>
2024-06-22 16:33:32 +0700

    nuxtjs + typescript + eslint + primevue + tailwindcss

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from "path";
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  modules: ["@primevue/nuxt-module", "@nuxtjs/eslint-module"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  primevue: {
+    options: {
+      unstyled: true,
+    },
+    importPT: { from: path.resolve(__dirname, "./presets/aura/") },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const count = ref(0);
+</script>
+
+<template>
+  <div class="flex h-screen w-screen items-center justify-center gap-2">
+    <Button @click="count--"> - </Button>
+    {{ count }}
+    <Button @click="count++"> + </Button>
+  </div>
+</template>
